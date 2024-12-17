3fe539e34eea7128e9568e93d063465246f9cd10
Julian Munera <julian.564915@gmail.com>
2024-12-17 17:03:19 -0500

    Arreglando error en donde el usuario podía actualizar una tarea con datos inválidos

--- Backend/src/main/java/com/juliobro/ready/domain/models/tarea/dto/ActualizarTareaDTO.java
+++ Backend/src/main/java/com/juliobro/ready/domain/models/tarea/dto/ActualizarTareaDTO.java
@@ -1,13 +1,22 @@
 package com.juliobro.ready.domain.models.tarea.dto;
 
 import com.juliobro.ready.domain.models.tarea.Estado;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
 public record ActualizarTareaDTO(
+
+        @NotBlank(message = "{validation.titulo}")
         String titulo,
-        String descripcion,
+
+        @NotNull(message = "{validation.fecha-limite}")
+        @Future(message = "{validation.fecha-limite-futura}")
         LocalDateTime fechaLimite,
+
+        String descripcion,
         Estado estado
 ) {
 }
